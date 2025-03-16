dfbd9cdcf0b4396c3159cb96972e32f135957b42
morings <78462305+morning-sketch@users.noreply.github.com>
2025-03-16 11:12:40 +0800

    取消解释集的限制

--- causalitylab/causal_reasoning/cleann_explainer.py
+++ causalitylab/causal_reasoning/cleann_explainer.py
@@ -79,12 +79,9 @@ class CLEANN:
             max_size = max_set_size
 
         explanations_list = []
-        for set_size in range(2, int(max_size/2) + 1):
-            sets_list = pds_tree.get_subsets_list(set_nodes=full_explain_set, subset_size=set_size)
-            sets_list.sort(key=lambda x: x[1])  # sort with respect to the sum of minimal distances
-            for possible_explanation_set in sets_list:
-                explanations_list.append(possible_explanation_set)
-                break
+        if self.is_explanation is None:
+            if len(full_explain_set) <= max_size:
+                explanations_list.append([full_explain_set, max_size])
         results['explanations'] = explanations_list
         self.results[target_node_idx] = results
         return explanations_list
